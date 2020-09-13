ff68d776984884c3ee4e2176fa999932d8d542ca
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-13 04:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function add() {
-  
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+
+function add(a, b) {
+  a + b;
+}
+
+function subtract(a, b) {
+  a - b;
+}
+
+function multiply(a, b) {
+  a * b
+}
+
+function divide(a, b) {
+  a / b
 }
